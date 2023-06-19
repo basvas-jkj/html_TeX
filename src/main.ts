dfd7928d7fdd5605b91a5a0808c02ae8fccca01b
basvas j.k.j <vasstupka@seznam.cz>
2023-06-19 09:23:48 +0200

    Základ tokenizace.
    Podpora stavů: data, tag_open, tag_close_open, tag_name, self_closing_start_tag, bogus_comment
    
    Podpora tokenů: character, eof, start_tag, end_tag, comment

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+﻿import * as fs from "node:fs";
+import {TOKENISER, TOKEN} from "./tokeniser"
+
+function on_parse_error(message: string): void
+{
+    console.error(message);
+}
+function on_token(t: TOKEN)
+{
+    if (t.type != "ch")
+        console.log(t.to_string());
+}
+
+function main(): void
+{
+    const source = fs.readFileSync("ex.html", "utf-8");
+    const t = new TOKENISER(on_parse_error, on_token);
+    t.tokenise(source);
+}
+
+process.on("uncaughtException", (e)=>
+{
+    console.error("Uncaught exception: " + e.message);
+})
+
+main();
