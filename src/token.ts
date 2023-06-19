8a9ae81d30a3a564ed5a41b99650c83a3198c810
basvas j.k.j <vasstupka@seznam.cz>
2023-06-19 13:00:43 +0200

    Přesun třídy TOKEN do samostatného souboru.

---
+++ src/token.ts
@@ -0,0 +1,27 @@
+export enum TOKEN_TYPE
+{
+    character = "ch", eof = "EOF", start_tag = "st", end_tag = "et", comment = "c"
+}
+
+export class TOKEN
+{
+    public readonly type: TOKEN_TYPE;
+    private content: string;
+    public self_closing: boolean;
+    
+    public constructor(type: TOKEN_TYPE, content = "")
+    {
+        this.type = type;
+        this.content = content;
+        this.self_closing = false;
+    }
+
+    public to_string(): string
+    {
+        return `<${this.type}> ${this.content}`;
+    }
+    public add(c: string): void
+    {
+        this.content += c;
+    }
+}
