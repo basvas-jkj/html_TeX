a2d14fe037b0a5a03b7918584497b72cab9d5c4c
basvas j.k.j <vasstupka@seznam.cz>
2023-07-07 19:49:45 +0200

    Základ sestavování stromové struktury dokumentu.
    V tuto chvíli jsou podporované komentáře, tagy html, head, meta, title a body.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import * as fs from "node:fs";
 
 import {TREE} from "./tree";
-import {TOKEN} from "./token";
 import {BUFFER} from "./buffer";
 import {tokenise} from "./tokeniser";
 
@@ -9,12 +8,7 @@ function on_parse_error(message: string): void
 {
     console.error(message);
 }
-function on_token(t: TOKEN)
-{
-    if (t.type != "ch")
-        console.log(t.to_string());
-}
-function on_uncaught_exception(e: Error)
+function on_uncaught_exception(e: Error): void
 {
     console.error("Uncaught exception: " + e.message);
 }
@@ -23,7 +17,8 @@ function main(): void
 {
     const source = fs.readFileSync("ex.html", "utf-8");
     const b = new BUFFER(source);
-    tokenise(b, on_parse_error, on_token);
+    const t = new TREE(on_parse_error);
+    tokenise(b, on_parse_error, t.receive_token.bind(t));
 }
 
 process.on("uncaughtException", on_uncaught_exception);
